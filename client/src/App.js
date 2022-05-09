5a5576eb630e5430ce6fcd47ab8da80b9c1914cb
Matthew McGrath <rubywep@hotmail.com>
2022-05-09 13:07:44 -0700

    Initial create-react-app setup and cleaning

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>React eCommerce - Beginning of an amazing site!</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
