8b5c3f68298996b3656501e774b46b0cdfbf6344
Matthew McGrath <rubywep@hotmail.com>
2022-05-09 18:45:17 -0700

    Add temporary list of products
    In the future, product information will be stored on the backend!

---
+++ client/src/products.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Rubber Batman Ducky',
+    description:
+      'Batman as a rubber ducky. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. We still need some more filler description text! So here is even more filler description text. We still have a little bit to go! Here is some filler description text. Here is the final bit of filler description text!',
+    image: '/images/products/toy-batduck.jpg',
+    category: 'Toy',
+    price: 69.42,
+    numberOfReviews: 8,
+    averageRating: 4.4,
+    numberOfStock: 25,
+  },
+  {
+    _id: '2',
+    name: 'Cute Kitty',
+    description:
+      'Batman as a rubber ducky. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. We still need some more filler description text! So here is even more filler description text. We still have a little bit to go! Here is some filler description text. Here is the final bit of filler description text!',
+    image: '/images/products/toy-cat.jpg',
+    category: 'Toy',
+    price: 69.42,
+    numberOfReviews: 8,
+    averageRating: 4.1,
+    numberOfStock: 42,
+  },
+  {
+    _id: '3',
+    name: 'Deadpool with Selfie',
+    description:
+      'Deadpool taking a selfie. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. We still need some more filler description text! So here is even more filler description text. We still have a little bit to go! Here is some filler description text. Here is the final bit of filler description text!',
+    image: '/images/products/toy-deadpool.jpg',
+    category: 'Clothing',
+    price: 69.42,
+    numberOfReviews: 8,
+    averageRating: 4.1,
+    numberOfStock: 95,
+  },
+  {
+    _id: '4',
+    name: 'Bob Ross',
+    description:
+      'No mistakes here! Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. We still need some more filler description text! So here is even more filler description text. We still have a little bit to go! Here is some filler description text. Here is the final bit of filler description text!',
+    image: '/images/products/toy-bobross.jpg',
+    category: 'Clothing',
+    price: 69.42,
+    numberOfReviews: 8,
+    averageRating: 4.1,
+    numberOfStock: 95,
+  },
+  {
+    _id: '5',
+    name: 'Rick n Morty',
+    description:
+      'High IQ required. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. We still need some more filler description text! So here is even more filler description text. We still have a little bit to go! Here is some filler description text. Here is the final bit of filler description text!',
+    image: '/images/products/toy-ricknmorty.jpg',
+    category: 'Clothing',
+    price: 69.42,
+    numberOfReviews: 8,
+    averageRating: 4.1,
+    numberOfStock: 95,
+  },
+  {
+    _id: '6',
+    name: 'Corpse Bride',
+    description:
+      'Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. We still need some more filler description text! So here is even more filler description text. We still have a little bit to go! Here is some filler description text. Here is the final bit of filler description text!',
+    image: '/images/products/toy-corpsebride.jpg',
+    category: 'Clothing',
+    price: 69.42,
+    numberOfReviews: 8,
+    averageRating: 4.1,
+    numberOfStock: 95,
+  },
+  {
+    _id: '7',
+    name: 'Mario',
+    description:
+      'Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. We still need some more filler description text! So here is even more filler description text. We still have a little bit to go! Here is some filler description text. Here is the final bit of filler description text!',
+    image: '/images/products/toy-mario.jpg',
+    category: 'Clothing',
+    price: 69.42,
+    numberOfReviews: 8,
+    averageRating: 4.1,
+    numberOfStock: 95,
+  },
+  {
+    _id: '8',
+    name: 'Name 8',
+    description:
+      'Batman as a rubber ducky. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. Here is some more filler description text. We still need some more filler description text! So here is even more filler description text. We still have a little bit to go! Here is some filler description text. Here is the final bit of filler description text!',
+    image: '/images/products/toy-batduck.jpg',
+    category: 'Clothing',
+    price: 69.42,
+    numberOfReviews: 8,
+    averageRating: 4.1,
+    numberOfStock: 95,
+  },
+];
+
+export default products;
