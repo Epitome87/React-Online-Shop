9b459c232d8aa30e14d2726537718a80c85c64c2
Matthew McGrath <rubywep@hotmail.com>
2022-05-10 22:40:06 -0700

    Prefix routes with 'api/', fix PORT variable
    PORT was accidentally being given the value of process.env instead of process.env.PORT

--- server/server.js
+++ server/server.js
@@ -4,12 +4,12 @@ import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 
 dotenv.config();
-const port = process.env || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 const products = { name: "matthew" };
 
-app.use('/users', userRouter);
-app.use('/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 
 app.get('/', (req, res) => {
     res.send('Home route');
