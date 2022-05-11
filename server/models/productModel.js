cf68321597201c756eb437b8613a72ec16bd9037
Matthew McGrath <rubywep@hotmail.com>
2022-05-10 21:35:20 -0700

    Begin Product model

---
+++ server/models/productModel.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true,
+        default: 0
+    },
+    category: {
+        type: String,
+        required: true,
+        default: 'Misc'
+    },
+    averageRating: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    numberOfRatings: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    numberOfStock: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
