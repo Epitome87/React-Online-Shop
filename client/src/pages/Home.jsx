b602888b90e7d3df4505b04c9195882b5d39e0e1
Matthew McGrath <rubywep@hotmail.com>
2022-05-11 23:25:47 -0700

    Begin refactor from local Product state to Redux

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,21 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { Card, Container, Grid, Row, Text } from '@nextui-org/react';
 import axios from 'axios';
 import ProductPreview from '../components/ProductPreview';
+import { useSelector, useDispatch } from 'react-redux'
+// import { retrieve } from '../redux/productsReducer';
+import { getProducts } from '../redux/productsReducer';
 
 /* Home Screen
 This screen will be a mini-landing page of sorts. The latest products and the top-reviewed products will be showcased here.
 Some information about the site will also be provided, and perhaps a nice (but small) hero section.
 */
 function Home() {
-  const [products, setProducts] = useState([]);
+  const { products } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get(`/api/products`);
-      setProducts(data);
-    }
-
-    fetchProducts();
+    dispatch(getProducts());
   }, []);
 
   return (
