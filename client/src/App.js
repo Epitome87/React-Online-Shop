bd0d006355826f3f2d62c44b4e58e0ca8e48a24f
Matthew McGrath <rubywep@hotmail.com>
2022-05-11 23:24:37 -0700

    Configure Redux Toolkit store, add Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 
 import { FAQ, Home, Product } from './pages';
 import { Footer, Header } from './components/layout';
+
+import productsReducer from './redux/productsReducer';
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+})
 
 const darkTheme = createTheme({
   type: 'dark',
@@ -14,17 +24,19 @@ const darkTheme = createTheme({
 
 function App() {
   return (
-    <NextUIProvider theme={darkTheme}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/products/:id' element={<Product />} />
-          <Route path='/faq' element={<FAQ />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </NextUIProvider>
+    <Provider store={store}>
+      <NextUIProvider theme={darkTheme}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/products/:id' element={<Product />} />
+            <Route path='/faq' element={<FAQ />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </NextUIProvider>
+    </Provider>
   );
 }
 
