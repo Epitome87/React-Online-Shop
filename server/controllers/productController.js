3d8f0d1d16047a32edde92f86ec8ccebe081e020
Matthew McGrath <rubywep@hotmail.com>
2022-05-14 21:31:34 -0700

    Develop Controller logic further

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,6 @@
 import Product from '../models/productModel.js';
 
-const getProducts = async (req, res) => {
+export const getProducts = async (req, res) => {
   try {
     const allProducts = await Product.find({});
     return res.status(200).send(allProducts);
@@ -9,7 +9,7 @@ const getProducts = async (req, res) => {
   }
 };
 
-const getProduct = async (req, res) => {
+export const getProduct = async (req, res) => {
   const { id: productID } = req.params;
 
   try {
@@ -25,4 +25,70 @@ const getProduct = async (req, res) => {
   }
 };
 
-export { getProducts, getProduct };
+export const updateProduct = async (req, res) => {
+  const propertiesToUpdate = Object.keys(req.body);
+  const allowedUpdates = ['name', 'price', 'description', 'image', 'category', 'numberInStock'];
+  const isValidOperation = propertiesToUpdate.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid Product updates ' });
+  }
+
+  try {
+    const product = await Product.findById(req.params.id);
+
+    for (const update of propertiesToUpdate) {
+      product[update] = req.body[update];
+    }
+
+    await product.save();
+
+    res.status(200).send(req.user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const createProductReview = async (req, res) => {
+  const { rating, comment } = req.body;
+
+  try {
+    // The product we are reviewing is part of the URL
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      // User has already submitted Review?
+      const userAlreadyReviewed = product.reviews.find((review) => review.user.toString() === req.user._id.toString());
+      if (userAlreadyReviewed) {
+        res.status(400);
+        throw new Error('Already reviewed this product');
+      }
+
+      const newReview = {
+        name: req.user.name,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+      };
+
+      product.reviews.push(newReview);
+
+      // TODO: Build this into the Product Model itself
+      product.numberOfRatings = product.reviews.length;
+
+      product.averageRating =
+        product.reviews.reduce((acc, curr) => {
+          return acc + curr.rating;
+        }, 0) / product.reviews.length;
+
+      await product.save();
+
+      res.status(201).json({ message: 'Review successfully added' });
+    } else {
+      res.status(404);
+      throw new Error('Product not found');
+    }
+  } catch (error) {
+    res.send(error.message);
+  }
+};
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,91 @@
 import User from '../models/userModel.js';
-import users from '../users.js';
 
-// Authenticate User, retrieve Token
+// Authenticate User (log in), retrieve Token
 export const authenticateUser = async (req, res) => {
-  const { email, name, password } = req.body;
-  // TODO: Use JSON Web Tokens
-  const token = 'okay!';
+  const { email, password } = req.body;
 
   try {
     const user = await User.findByCredentials(email, password);
+
+    // Generate Auth Token for this user
+    const token = await user.generateAuthToken();
+
     return res.status(200).send({ user, token });
   } catch (error) {
     res.status(401).send(error.message);
   }
 };
 
-export const getUser = async (req, res) => {
-  res.json(users);
+// Authenticate User, retrieve Token
+export const registerUser = async (req, res) => {
+  const { email, name, password } = req.body;
+
+  try {
+    const userAlreadyRegistered = await User.findOne({ email });
+
+    // This User exists already -- can't re-register!
+    if (userAlreadyRegistered) {
+      res.status(400);
+      throw new Error('User already registered');
+    }
+
+    // Despite its name, .create will also allow our pre 'save' User middleware to run
+    // Our password will be hashed in that middleware -- no need to do it here!
+    const user = await User.create({
+      email,
+      name,
+      password,
+    });
+
+    // Everything went smooth creating the User...
+    if (user) {
+      // Generate Auth Token for this User
+      const token = await user.generateAuthToken();
+
+      res.status(201).json({
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+        isAdmin: user.isAdmin,
+        token,
+      });
+    } else {
+      throw new Error('Error attempting to register');
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+
+  if (!user) {
+    res.status(404).send('User not found');
+  }
+
+  res.send(user);
+};
+
+export const updateUserProfile = async (req, res) => {
+  const propertiesToUpdate = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password'];
+  const isValidOperation = propertiesToUpdate.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid User updates ' });
+  }
+
+  try {
+    for (const update of propertiesToUpdate) {
+      req.user[update] = req.body[update];
+    }
+
+    // Remember: Password is encrypted pre-save by our User Schema!
+    await req.user.save();
+
+    res.status(200).send(req.user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
