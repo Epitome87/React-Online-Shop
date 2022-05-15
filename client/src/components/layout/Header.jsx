3d9ac959d2d0c2a895288f6f05b903495faaf886
Matthew McGrath <rubywep@hotmail.com>
2022-05-14 22:02:49 -0700

    Add conditional rendering of Login/Logout/Profile

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout } from '../../redux/userSlice';
 import { Container, Card, Grid, Row, Text } from '@nextui-org/react';
 
 function Header() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
+
+  const onLogout = (event) => {
+    dispatch(logout());
+  };
+
+  let renderedContent;
+
+  renderedContent = user ? (
+    <React.Fragment>
+      <Grid xs>
+        <NavLink to='#' onClick={onLogout}>
+          Sign Out
+        </NavLink>
+      </Grid>
+      <Grid xs>
+        <NavLink to='#' onClick={onLogout}>
+          Profile {user.name}
+        </NavLink>
+      </Grid>
+    </React.Fragment>
+  ) : (
+    <Grid xs>
+      <NavLink to='/login'>Sign In</NavLink>
+    </Grid>
+  );
+
   return (
     <header>
       <Container>
@@ -17,6 +47,7 @@ function Header() {
             <Grid xs>
               <NavLink to='#'>FAQ</NavLink>
             </Grid>
+            {renderedContent}
           </Grid.Container>
         </Row>
       </Container>
