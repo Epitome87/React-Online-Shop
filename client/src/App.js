1356d388f8dc022347e69edb2bfe2ef24648b768
Matthew McGrath <rubywep@hotmail.com>
2022-05-14 22:01:45 -0700

    Add userReducer, routes for Profile/Login/Register

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,22 @@ import { NextUIProvider, createTheme } from '@nextui-org/react';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 
-import { FAQ, Home, Product, ShoppingCart } from './pages';
+// Import our Page components and Layout components
+import { FAQ, Home, Login, Product, Profile, Registration, ShoppingCart } from './pages';
 import { Footer, Header } from './components/layout';
 
+// Import our Reducers
 import productsReducer from './redux/productSlice';
 import productDetailsReducer from './redux/productDetailsSlice';
 import cartReducer from './redux/cartSlice';
+import userReducer from './redux/userSlice';
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    user: userReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
@@ -39,6 +43,9 @@ function App() {
             <Route path='/cart/' element={<ShoppingCart />} />
             <Route path='/cart/:id' element={<ShoppingCart />} />
             <Route path='/faq' element={<FAQ />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/registration' element={<Registration />} />
+            <Route path='/profile' element={<Profile />} />
           </Routes>
           <Footer />
         </Router>
